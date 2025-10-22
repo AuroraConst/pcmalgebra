3aea3bfb4e7d8836da204374454a3e556eccea15
AuroraConst <69015147+AuroraConst@users.noreply.github.com>
2025-10-22 10:06:33 -0400

    Rough testing ideas

--- src/test/scala/org/aurora/sjsast/ParseTest.scala
+++ src/test/scala/org/aurora/sjsast/ParseTest.scala
@@ -6,8 +6,10 @@ class ParseTest extends BaseAsyncTest:
   
   
 
-  "First Parse" should {
-    "work" in { 
+  "Parse" should {
+    "parse file correctly" in { 
+      //TODO note if there is no file named Parse-0.aurora, it will automatically create one
+      //SHOULD TEST FILE NAMING BE MORE DESCRIPTIVE e.g. Parse-Valid.aurora
       val path = testfilepath(0)
 
       for {
@@ -19,4 +21,54 @@ class ParseTest extends BaseAsyncTest:
       }
     }
 
+
+
+    "parse(1) correctly" in { 
+      val path = testfilepath(1)
+
+      for {
+        result <- parse(1)
+         _     <- Future(info(s"result: $result"))
+        //TODO HOW DO YOU REPRESENT CATCHING EXCEPTION 
+        b <- Future(true should be (true))
+
+
+      } yield {
+        b
+      }
+    }
+
+
+    "parse(3)is a module" in {
+      val path = testfilepath(3)
+
+      for {
+        pcm <- parse(3)
+        module <- Future(pcm.module.get.name)
+        //TODO CAN WE GET URI of resource?
+        //https://langium.org/docs/learn/workflow/resolve_cross_references/
+        uri    <- Future(module)
+        _      <- Future(info(s"module: $module"))
+        b <- Future(true should be (true))
+
+
+      } yield {
+        b
+      }
+    }
+
+
+
+    "parse(4) references module from parse(3) in {"  in {
+      val path3 = testfilepath(3)
+      val path4 = testfilepath(4)
+
+      for {
+        module <- Future(parse(3))
+        pcm4   <- parse(4)
+        b   <-    Future(true should be (true))
+       } yield {
+       b
+      }
   }
+} 
