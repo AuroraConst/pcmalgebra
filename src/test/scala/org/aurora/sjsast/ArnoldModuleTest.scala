f670d92a6411dfb7b9b014e81993c2ad60c09369
AuroraConst <69015147+AuroraConst@users.noreply.github.com>
2025-11-15 20:44:02 -0500

    Update ArnoldModuleTest.scala

--- src/test/scala/org/aurora/sjsast/ArnoldModuleTest.scala
+++ src/test/scala/org/aurora/sjsast/ArnoldModuleTest.scala
@@ -23,7 +23,7 @@ class ArnoldModuleTest extends BaseAsyncTest:
 
     "be a module" in { 
       //TODO note if there is no file named ArnoldModule-0.aurora, it will automatically create one
-      //SHOULD TEST FILE NAMING BE MORE DESCRIPTIVE e.g. Parse-Valid.aurora
+      //SHOULD TEST FILE NAMING BE MORE DESCRIPTIVE e.g. ArnoldModule-Valid.aurora
 
       for {
         astPCM      <- parse(0)
