f75aca142e5d3e579d50b6a5ec42ed967a627e80
AuroraConst <auroraconstellations@gmail.com>
2025-11-25 16:14:44 -0500

    refined test narratives

--- src/test/scala/org/aurora/sjsast/arnoldinterpreter/ArnoldInterpreterTest.scala
+++ src/test/scala/org/aurora/sjsast/arnoldinterpreter/ArnoldInterpreterTest.scala
@@ -5,19 +5,19 @@ import org.aurora.sjsast._
 class ArnoldInterpreterTest extends BaseAsyncTest:
 
   "evaluate" should {
-    "add" in { 
+    "5+3 should be 8" in { 
       for {
         result     <- Future(SimpleInterpreter.evaluate(Add(Number(5), Number(3))))
       } yield result should be(IntValue(8))
     }
 
-    "less than" in {
+    "2 should be < 5" in {
       for {
         result     <- Future(SimpleInterpreter.evaluate(LessThan(Number(2), Number(5))))
       } yield result should be(BoolValue(true))
     }
 
-    "add numbers then less than" in {
+    "2 + 2 should be less than 5" in {
       for {
         result     <- Future(SimpleInterpreter.evaluate(LessThan(Add(Number(2),Number(2)), Number(5))))
       } yield result should be(BoolValue(true))
