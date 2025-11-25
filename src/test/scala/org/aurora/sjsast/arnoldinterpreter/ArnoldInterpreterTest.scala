ff620c5f9379a1d5d8d02a1fc74da02c2ea0c991
AuroraConst <auroraconstellations@gmail.com>
2025-11-25 16:01:17 -0500

    additional expressions for testing

--- src/test/scala/org/aurora/sjsast/arnoldinterpreter/ArnoldInterpreterTest.scala
+++ src/test/scala/org/aurora/sjsast/arnoldinterpreter/ArnoldInterpreterTest.scala
@@ -5,9 +5,21 @@ import org.aurora.sjsast._
 class ArnoldInterpreterTest extends BaseAsyncTest:
 
   "evaluate" should {
-    "work" in { 
+    "add" in { 
       for {
         result     <- Future(SimpleInterpreter.evaluate(Add(Number(5), Number(3))))
       } yield result should be(IntValue(8))
-    } 
+    }
+
+    "less than" in {
+      for {
+        result     <- Future(SimpleInterpreter.evaluate(LessThan(Number(2), Number(5))))
+      } yield result should be(BoolValue(true))
+    }
+
+    "add numbers then less than" in {
+      for {
+        result     <- Future(SimpleInterpreter.evaluate(LessThan(Add(Number(2),Number(2)), Number(5))))
+      } yield result should be(BoolValue(true))
+    }
   }
